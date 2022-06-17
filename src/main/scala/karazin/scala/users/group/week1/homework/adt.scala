4a55c79111c150c6dbffeec5e17f3f0f2675f8ca
tynuk <bowsworddv@gmail.com>
2022-06-17 17:11:45 +0300

    copy apply from week1

--- src/main/scala/karazin/scala/users/group/week1/homework/adt.scala
+++ src/main/scala/karazin/scala/users/group/week1/homework/adt.scala
@@ -48,6 +48,5 @@ object adt:
       Make sure that in case of failing the method with exception
       no exception is thrown but the case for an error is returned
     */
-    def apply = ???
-      
-  
+    def apply[V](v: V): ErrorOr[V] =
+      if v == null then ErrorOr.Fail(Exception("Null ptr")) else ErrorOr.Value(v)
